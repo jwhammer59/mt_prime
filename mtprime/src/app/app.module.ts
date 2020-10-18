ef5bb0c91835be77afaf2b4de58e472b1963037a
Jeff Hammer <jeff@jhammermedia.com>
2020-10-18 17:17:40 -0400

    add angular-fire and firebase

--- mtprime/src/app/app.module.ts
+++ mtprime/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './routing/app-routing.module';
 
 // Angular Fire Modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 // Prime NG CSS Library Modules
 import { PrimeModule} from 'src/app/prime-ng/prime.module';
@@ -17,7 +22,8 @@ import { RegisterComponent } from './components/auth/register/register.component
 import { HomeComponent } from './components/core/home/home.component';
 
 // Auth Modules
-
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './components/auth/auth-guard';
 
 
 @NgModule({
@@ -32,11 +38,17 @@ import { HomeComponent } from './components/core/home/home.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    FormsModule,
     PrimeModule,
     SharedModule
 
   ],
-  providers: [],
+  exports: [AppRoutingModule],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
